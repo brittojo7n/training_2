e61b9b06332a9d9d59118af1ab0e5ec20f34e9ac
brittojo7n <sjbritto2003@mail.com>
2025-07-14 14:24:48 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let login = true;
+  let text;
+  
+  if (login === true){
+    text = "Logged in";
+  } else {
+    text = "Not logged in";
+  }
   return (
     <div class="App">
       <header>
       <h1>Welcome!</h1>
+      <h2>{text}</h2>
       </header>
       <img class="App-logo" src={logo} alt="logo" />
     </div>
