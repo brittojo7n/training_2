5585777dea68df9149665367cb5f21b35da5c390
brittojo7n <sjbritto2003@mail.com>
2025-07-14 11:47:28 +0530

    created

---
+++ src/components/Prop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Prop.css';
+
+function Prop() {
+  return (
+    <div>
+        <h3>This is `Prop.js` component</h3>
+        
+    </div>
+  )
+}
+
+export default Prop;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Prop from './components/Prop.js'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div>
     <App />
+    <Prop />
+    </div>
   </React.StrictMode>
 );
 
