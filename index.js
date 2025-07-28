6dea1702ba7869a15e89e74d4a4e3b8451ea569a
brittojo7n <sjbritto2003@mail.com>
2025-07-28 15:03:14 +0530

    commit

--- index.js
+++ index.js
@@ -26,15 +26,9 @@ const server = http.createServer((req, res) => {
             body += chunk.toString();
         });
         req.on('end', () => {
-            try {
-                const data = JSON.parse(body);
-                console.log('Received data:', data);
-                res.writeHead(201, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ message: 'Data received successfully', yourData: data }));
-            } catch (error) {
-                res.writeHead(400, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ message: 'Bad Request: Invalid JSON' }));
-            }
+            const data = JSON.parse(body);
+            console.log('Received data:', data);
+            res.end();
         });
     }
 
