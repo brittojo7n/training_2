fed1cb091ed35600b65a4544e08c1b29a14a2292
brittojo7n <sjbritto2003@mail.com>
2025-07-28 11:43:57 +0530

    commit

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello!!!\n');
+    // log incoming requests to console
+    console.log(`Incoming Request: ${req.method} ${req.url}`);
+
+    // homepage
+    if (req.url === '/' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Welcome to the Homepage!</h1><p>Try navigating to /about</p>');
+    } 
+    
+    // about page
+    else if (req.url === '/about' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ 
+            message: 'This is the About page.',
+            timestamp: new Date()
+        }));
+    } 
+
+    // Page Not Found
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404</h1><h2>The requested page was not found</h2>');
+    }
 });
 
-server.listen(3000);
+const PORT = 3000;
+const HOSTNAME = '127.0.0.1'; 
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`🚀 Server running at http://${HOSTNAME}:${PORT}/`);
+});
